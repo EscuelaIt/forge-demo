b1db605cbf9e723ed17bab2aa7e68ee3ce76b392
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-11-24 15:28:57 +0100

    task de horizon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:log-time')->everyFiveSeconds();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
